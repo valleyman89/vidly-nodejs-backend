3b61432edf30f96faed05839a8a06b300f00a4a3
valleyman89 <dev@stevenrolph.com>
2022-03-31 15:20:13 +0100

    update port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./startup/routes")(app);
 require("./startup/validation")();
 
 // LISTEN
-const port = process.env.port || 5005;
+const port = process.env.port;
 const server = app.listen(port, () => {
   winston.info(`Listening on port: ${port}.`);
 });
